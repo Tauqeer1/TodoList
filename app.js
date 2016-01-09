296f8e28781758316aa76cee57d3222259b7a666
tauqeer <tauqeer.shakir@yahoo.com>
2016-01-09 17:47:35 +0500

    initial

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Tauqeer Ahmed on 1/8/2016.
+ */
+
+
+var app = angular.module('mainApp',['ngMaterial']);
+
+app.controller('myCtrl',function($scope){
+
+    $scope.itemsArray = [];
+    $scope.AddToTodoList = function(){
+
+        if($scope.todoitem){
+            //$scope.itemArray.push($scope.todoitem);
+            $scope.itemsArray.push({item : $scope.todoitem , delete : false});
+            $scope.todoitem = '';
+            $scope.RemainingItemInList();
+        }
+    };
+
+    $scope.RemainingItemInList = function(){
+
+        $scope.count = $scope.itemsArray.length;
+
+        for(var i=0;i<$scope.itemsArray.length;i++){
+
+            if($scope.itemsArray[i].delete){
+                $scope.count--;
+            }
+        }
+    };
+    $scope.DeleteSelectedItems = function(){
+
+    };
+
+
+});
